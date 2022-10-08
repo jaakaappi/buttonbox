329c3697eb32cedf9d67cb244e79a31585e2e879
Esa Niemi <esa.niemi@protonmail.com>
2022-10-08 11:38:02 +0300

    Changed to micro/leonardo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# joystick
+# buttonbox
 
-Easily configurable Teensy HID joystick
+Easily configurable HID button box
 
 Supports 32 buttons, 2 sliders and 4 axes
 
@@ -8,6 +8,6 @@ Supports 32 buttons, 2 sliders and 4 axes
 
 - Install Platform.IO plugin to VSCode
 - Open folder in VSCode as a Platform.IO project
-- In `src/main.cpp` fill in the pin numbers for [HID joystick](https://www.pjrc.com/teensy/td_joystick.html) buttons, axes and sliders. Digital pins for buttons, analog for axes and sliders. See [Teensy pinout](https://www.pjrc.com/teensy/pinout.html) for available pin numbers
+- In `src/main.cpp` fill in the pin numbers for joystick buttons, axes and sliders. Digital pins for buttons, analog for axes and sliders. If you need a new board, check the Platform.io board identifier and add a new build option to platformio.ini file
 - Upload to board
-- Linked HID joystick page has instructions for seeing joystick output
+- On Windows you can best see the output by writing "gamepad" into Win search > "Set up USB game controllers" > your controller > properties
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,34 @@
 #include <Arduino.h>
+#include "Joystick.h"
 
-// Button pins, up to 32. Number of the button is place in array + 1
-const int BUTTONS[] = {0};
+// Button pins, up to 32. Number of the button is place in array, 0 => 1
+const int BUTTONS[] = {PD2, PD3, PD6};
+
+// Pins for switches which will work like buttons, pressing the button for SWITCH_BUTTON_DELAY after being flipped
+// Swiotch buttons will get numbers after normal buttons, so BUTTONS.length + switch button index
+const int SWITCH_BUTTON_DELAY = 500;
+const int SWITCH_BUTTONS[] = {PD4, PD5};
+
 // Joystick sliders, up to 2
 const int SLIDERS[] = {};
+
 // Joystick axes analog pins in order: X, Y, Z, Z rotate
-const int AXES[] = {A9};
+const int AXES[] = {};
 
-void setup()
-{
-  Serial.begin(9600);
+Joystick_ Joystick(0x06,
+                   JOYSTICK_TYPE_JOYSTICK, 32, 0,
+                   true, true, true, true, false, false,
+                   true, true, false, false, false);
 
-  for (size_t i = 0; i < sizeof(BUTTONS) / sizeof(BUTTONS[0]); i++)
-  {
-    pinMode(BUTTONS[i], INPUT_PULLUP);
-  }
-}
+int *switch_button_delays;
+bool *switch_button_states;
 
 void update_buttons()
 {
   for (size_t i = 0; i < sizeof(BUTTONS) / sizeof(BUTTONS[0]); i++)
   {
     boolean pressed = !digitalRead(BUTTONS[i]); // Digital inputs have a pullup resistor => pressed = 0
-
-    Serial.println("Button " + String(i + 1) + " " + String(pressed));
-
-    Joystick.button(i + 1, pressed);
+    Joystick.setButton(i, pressed);
   }
 }
 
@@ -34,8 +37,8 @@ void update_sliders()
   for (size_t i = 0; i < sizeof(SLIDERS) / sizeof(SLIDERS[0]); i++)
   {
     int value = analogRead(SLIDERS[i]);
-    Serial.println("Slider " + String(i + 1) + " " + String(value));
-    i == 0 ? Joystick.sliderLeft(value) : Joystick.sliderRight(value);
+    // Serial.println("Slider " + String(i + 1) + " " + String(value));
+    i == 0 ? Joystick.setThrottle(value) : Joystick.setRudder(value);
   }
 }
 
@@ -44,20 +47,20 @@ void update_axes()
   for (size_t i = 0; i < sizeof(AXES) / sizeof(AXES[0]); i++)
   {
     int value = analogRead(AXES[i]);
-    Serial.println("Axis " + String(i + 1) + " " + String(value));
+    // Serial.println("Axis " + String(i + 1) + " " + String(value));
     switch (i)
     {
     case 0:
-      Joystick.X(value);
+      Joystick.setXAxis(value);
       break;
     case 1:
-      Joystick.Y(value);
+      Joystick.setYAxis(value);
       break;
     case 2:
-      Joystick.Z(value);
+      Joystick.setZAxis(value);
       break;
     case 3:
-      Joystick.Zrotate(value);
+      Joystick.setRxAxis(value);
       break;
 
     default:
@@ -66,10 +69,69 @@ void update_axes()
   }
 }
 
+void update_switch_buttons()
+{
+  const int BUTTONS_LENGTH = sizeof(BUTTONS) / sizeof(BUTTONS[0]);
+
+  for (size_t i = 0; i < sizeof(SWITCH_BUTTONS) / sizeof(SWITCH_BUTTONS[0]); i++)
+  {
+    bool pressed = digitalRead(SWITCH_BUTTONS[i]);
+    if (switch_button_delays[i] != 0)
+    {
+      if (millis() >= switch_button_delays[i])
+      {
+        switch_button_delays[i] = 0;
+        Joystick.setButton(BUTTONS_LENGTH + i, false);
+      }
+    }
+    if (pressed != switch_button_states[i])
+    {
+      switch_button_states[i] = pressed;
+      switch_button_delays[i] = millis() + SWITCH_BUTTON_DELAY;
+      Joystick.setButton(BUTTONS_LENGTH + i, true);
+    }
+  }
+}
+
+void setup()
+{
+  Joystick.setXAxisRange(-127, 127);
+  Joystick.setYAxisRange(-127, 127);
+  Joystick.setZAxisRange(-127, 127);
+  Joystick.setRxAxisRange(-127, 127);
+  Joystick.setThrottleRange(0, 255);
+  Joystick.setRudderRange(0, 255);
+
+  Joystick.begin();
+
+  for (size_t i = 0; i < sizeof(BUTTONS) / sizeof(BUTTONS[0]); i++)
+  {
+    pinMode(BUTTONS[i], INPUT_PULLUP);
+  }
+
+  for (size_t i = 0; i < sizeof(SWITCH_BUTTONS) / sizeof(SWITCH_BUTTONS[0]); i++)
+  {
+    pinMode(SWITCH_BUTTONS[i], INPUT_PULLUP);
+  }
+
+  switch_button_delays = new int[sizeof(SWITCH_BUTTONS) / sizeof(SWITCH_BUTTONS[0])];
+  switch_button_states = new bool[sizeof(SWITCH_BUTTONS) / sizeof(SWITCH_BUTTONS[0])];
+
+  for (size_t i = 0; i < sizeof(switch_button_delays) / sizeof(switch_button_delays[0]); i++)
+  {
+    switch_button_delays[i] = 0;
+  }
+
+  for (size_t i = 0; i < sizeof(switch_button_states) / sizeof(switch_button_states[0]); i++)
+  {
+    switch_button_states[i] = false;
+  }
+}
+
 void loop()
 {
   update_buttons();
+  update_switch_buttons();
   update_sliders();
   update_axes();
-  // delay(1000);
 }
